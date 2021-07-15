33343db72b8dca190cadef663f54024791f7a9b9
ysycecilia <ysycecilia@gmail.com>
2021-07-14 21:31:40 -0400

    cleanup code

--- src/components/Appointment/Error.js
+++ src/components/Appointment/Error.js
@@ -4,7 +4,7 @@ export default function Error(props){
   return(
     <main className="appointment__card appointment__card--error">
       <section className="appointment__error-message">
-        <h1 className="text--semi-bold">Error</h1>
+        <h1 className="text--semi-bold" alt="Error">Error</h1>
         <h3 className="text--light">{props.message}</h3>
       </section>
       <img
